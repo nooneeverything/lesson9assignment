ed7b4e28beb06af42948e4e501ac8f51ffbe8019
Noah <17791713+nooneeverything@users.noreply.github.com>
2023-07-12 02:26:22 -0700

    Add files via upload

---
+++ pages/pages/AboutUs.vue
@@ -0,0 +1,110 @@
+<template>
+<!-- Accordian Created Using V-For Loop Iterating Through Content-->
+  <b-container>
+    <div id="app">
+    <b-row>
+      <b-col>
+          <h2 class="mt-5"> About Us</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lacinia pharetra. Pellentesque diam turpis, ultrices et lorem id, sagittis pretium lacus. Sed tempor semper metus in cursus. Aliquam commodo suscipit erat, at sodales elit. Praesent viverra accumsan rutrum. Donec turpis dui, viverra quis turpis mattis, vestibulum vehicula orci. Ut commodo urna quam, nec ultrices neque posuere non. Phasellus nulla tellus, dapibus at suscipit ultrices, venenatis eu nibh. Vestibulum luctus auctor laoreet. Ut tempus imperdiet velit, eleifend pulvinar diam.
+                Nunc in velit ante. Sed vel elit est. Suspendisse tincidunt elit vel fermentum iaculis. Donec condimentum magna quis orci laoreet, nec vehicula velit placerat. Aliquam consectetur commodo massa, ut vulputate urna egestas et. Nullam a velit sit amet nulla eleifend vestibulum sed quis odio. Morbi eget finibus quam, sed porta dui. Nunc nec ante posuere, lacinia dui maximus, aliquet libero. Sed leo nulla, bibendum ac ipsum eu, varius eleifend sem. Nunc imperdiet id eros ac sagittis.
+              </p>
+                        
+      </b-col>
+    </b-row>
+
+    <div v-for="x in questions" :key="x">
+      <b-card-header>
+        <b-button block size="lg" v-b-toggle.collapse-1 variant="success" pill v-on:click="x.isOpen = !x.isOpen">{{x.question}}</b-button>
+      </b-card-header>
+      <b-card-body>
+        <b-card-text v-if="x.isOpen">{{x.answer}}</b-card-text>
+      </b-card-body>
+    </div>
+
+
+</div>
+
+<!--<div class="accordion" role="tablist">
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-1 variant="info">{{items.a1.question}}</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>{{ items.a1.answer }}</b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-2 variant="info">{{items.a2.question }}</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>{{items.a2.answer}}</b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-3 variant="info">{{ items.a3.question }}</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>{{items.a3.answer }}</b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+  </div> -->
+</b-container>
+
+</template>
+
+
+<script>
+
+
+  export default {
+    name: 'app',
+    data(){
+      return{
+        questions: {
+          item1: {
+            question: "Who are our typical clients?",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vestibulum odio purus, nec commodo lacus auctor vitae. Curabitur et interdum sem, ut egestas sapien. Integer sem magna, iaculis eget venenatis at, egestas non eros. Suspendisse erat ipsum, volutpat quis nisi sed, pulvinar vestibulum dui. Fusce porttitor iaculis malesuada. In dignissim sit amet ante nec rutrum. Pellentesque vel maximus diam. Ut id pellentesque libero, a rutrum ante. Donec id nulla diam.",
+            isOpen: "True"
+          },
+          item2: {
+            question: "How do our fees work?",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vestibulum odio purus, nec commodo lacus auctor vitae. Curabitur et interdum sem, ut egestas sapien. Integer sem magna, iaculis eget venenatis at, egestas non eros. Suspendisse erat ipsum, volutpat quis nisi sed, pulvinar vestibulum dui. Fusce porttitor iaculis malesuada. In dignissim sit amet ante nec rutrum. Pellentesque vel maximus diam. Ut id pellentesque libero, a rutrum ante. Donec id nulla diam.",
+            isOpen: "True"
+          },
+
+          item3: {
+            question: "What kind of services do we provide?",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vestibulum odio purus, nec commodo lacus auctor vitae. Curabitur et interdum sem, ut egestas sapien. Integer sem magna, iaculis eget venenatis at, egestas non eros. Suspendisse erat ipsum, volutpat quis nisi sed, pulvinar vestibulum dui. Fusce porttitor iaculis malesuada. In dignissim sit amet ante nec rutrum. Pellentesque vel maximus diam. Ut id pellentesque libero, a rutrum ante. Donec id nulla diam.",
+            isOpen: "True"
+          },
+
+        }
+
+    }
+    
+  },
+  head() {
+    return {
+      title: "About Us",
+      meta: [
+        {
+          hid: 'description',
+          name: 'decription',
+          content: 'History about the firm and more information'
+        }
+      ]
+    }
+  }
+}
+ 
+</script>
---
+++ pages/pages/Home.vue
@@ -0,0 +1,50 @@
+<template>
+    <!-- Homepage Content -->
+    <b-container>
+      
+              <b-row>
+
+                <b-col cols="6">
+                
+                  <h2 class="mt-5 mb-3"> Sidebar 1</h2>
+                  <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla malesuada ultricies tincidunt. Ut id risus ultricies, viverra ex in, sagittis urna. Aenean odio dolor, blandit sed metus et, maximus mollis dolor. Fusce quis hendrerit nisi, a aliquet eros. Praesent molestie, mi quis placerat eleifend, velit felis fringilla ex, eget consectetur ligula sem quis quam. Sed libero leo, sagittis quis sapien id, ullamcorper facilisis massa. Sed condimentum magna at laoreet mollis. Vestibulum ultrices elementum imperdiet. Morbi viverra nisl sed lectus elementum, eu imperdiet nunc egestas. Quisque eget mollis risus. Pellentesque leo nisi, consequat hendrerit elit vitae, varius commodo quam.</p>
+                  <h2 class="mt-5 mb-3"> Sidebar 2 </h2>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla malesuada ultricies tincidunt. Ut id risus ultricies, viverra ex in, sagittis urna. Aenean odio dolor, blandit sed metus et, maximus mollis dolor. Fusce quis hendrerit nisi, a aliquet eros. Praesent molestie, mi quis placerat eleifend, velit felis fringilla ex, eget consectetur ligula sem quis quam. Sed libero leo, sagittis quis sapien id, ullamcorper facilisis massa. Sed condimentum magna at laoreet mollis. Vestibulum ultrices elementum imperdiet. Morbi viverra nisl sed lectus elementum, eu imperdiet nunc egestas. Quisque eget mollis risus. Pellentesque leo nisi, consequat hendrerit elit vitae, varius commodo quam.
+              
+                </b-col>
+                
+
+                <b-col cols="6">
+                
+                  <h2 class="mt-5 mb-3"> Main Section</h2>
+                  <img src="#" alt="image of a lawyer working">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae erat quis massa viverra aliquam. Nunc condimentum leo non mattis porta. Vestibulum arcu ex, mattis nec aliquam et, ornare ut dui. Praesent finibus vestibulum ante, eu vulputate augue blandit in. Proin imperdiet nulla leo, ac pretium nisi viverra eu. Nulla facilisi. Duis congue mauris vitae congue mattis. Proin suscipit, neque et molestie consectetur, erat felis interdum dolor, in ullamcorper massa mauris in nibh. Fusce malesuada enim lorem, tincidunt aliquet dui pellentesque id. Praesent ullamcorper, augue fringilla tincidunt feugiat, justo turpis convallis est, ultricies pulvinar nisl diam vel sapien. Pellentesque orci dolor, tristique non porta non, cursus vitae odio. Donec felis mauris, aliquam vel augue non, eleifend rhoncus ipsum. Quisque feugiat feugiat pellentesque. Aliquam accumsan lorem rutrum ipsum pulvinar, eget sollicitudin sem mollis. Aliquam condimentum sem ut risus facilisis porta ut vitae lacus. Cras porta, sem ut euismod accumsan, neque magna dapibus nisl, vitae mollis orci ante eget sem.
+    
+                    Maecenas ultricies, dui sed dignissim mattis, massa sapien ultricies tellus, sit amet lobortis nisl dolor vitae augue. Quisque eget arcu vulputate massa vehicula dapibus. Donec tincidunt libero quis interdum venenatis. Cras ac ipsum metus. Proin non posuere dolor. Praesent blandit vestibulum ex, in sodales est efficitur aliquet. Nullam tempor tellus quis orci euismod vulputate. Integer laoreet tristique ante, in luctus massa imperdiet quis. Phasellus eu odio nec turpis commodo blandit ut et lectus. Sed gravida sapien magna, sed pretium leo vestibulum nec. Fusce tortor odio, dictum vel ligula sit amet, tempus faucibus nunc. Integer ut eros id tortor pharetra varius. Etiam congue ex a nunc molestie feugiat. Proin consequat in orci quis dictum.
+                    
+                    Integer sed leo vitae nulla euismod euismod sed et ligula. Fusce faucibus feugiat velit, ut pharetra mauris volutpat eget. Nullam dolor eros, aliquet nec tempus nec, aliquet eu dolor. Pellentesque efficitur elit non tortor accumsan vulputate. Sed vestibulum mattis ex et malesuada. Morbi efficitur nisi neque, a rutrum urna convallis non. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque eget facilisis metus, quis ultrices quam. Nulla eu consectetur nibh. Curabitur tellus eros, efficitur vitae pretium sed, pharetra pretium elit. Morbi posuere id leo sit amet eleifend. Mauris nisl erat, pharetra at venenatis a, imperdiet vitae orci. Donec commodo quis metus non interdum.</p>
+                      Tooltip on bottom
+                  
+                </b-col>
+              </b-row>
+      </b-container>
+            
+    
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Homepage",
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Homepage of website with basic information about law firm'
+        }
+      ]
+    }
+  }
+}
+</script>
