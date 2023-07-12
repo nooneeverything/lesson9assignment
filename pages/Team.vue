d9c76552b7f67e01c7ca584a2f92f5b908be4500
Noah <17791713+nooneeverything@users.noreply.github.com>
2023-07-12 02:28:46 -0700

    Update Team.vue

--- pages/Team.vue
+++ pages/Team.vue
@@ -14,19 +14,35 @@
 </template>
     
   <script>
+  
 
     export default {
+
       name: 'gallery', 
       data() {
-        return {
+        return {  
           images: [  
-            { src: require('../assets/images/image1.jpg'), name: 'Roberta Evenson'},
+            { src: require('../assets/images/image1.jpg'), name: 'Robertaz Evenson'},
             { src: require('../assets/images/image2.jpg'), name: 'Robert Peterson'},
             { src: require('../assets/images/image3.jpg'), name: 'Rebecca Stevens'},
           ],
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut turpis enim, convallis at mi sed, finibus scelerisque felis. Etiam consequat nisi dui, venenatis porta erat consectetur eu. In sed lectus erat. Donec porttitor velit nec massa varius rhoncus. Vivamus orci nunc, commodo non mollis in, tristique sed neque. Aenean sollicitudin scelerisque mi quis pretium. In malesuada mauris sed ante ullamcorper lobortis. Duis erat nisi, varius sed bibendum non, vulputate et sem. Vestibulum ac mattis tortor. '
+        
         }
-      }
+
+  },
+  head() {
+    return {
+      title: "Team",
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Information about the lawyers at the firm'
+        }
+      ]
     }
+  }
+}   
     
   </script>
